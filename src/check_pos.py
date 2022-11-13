aa05698a614498fe745f8fe2fe0d66e9c529a94a
Jonnas Costa <jonn567@gmail.com>
2022-11-13 02:12:57 +0000

    fix major bug on "save btn"
    do not record a click when click save macro
    the bug: the click ignore wasn't working.

--- src/check_pos.py
+++ src/check_pos.py
@@ -1,16 +1,19 @@
 #! python3
 import json
-from threading import Thread
 import time
-from tkinter import messagebox, ttk
-from typing import List
-import pyautogui
 import tkinter as tk
-from tkinter import Label, StringVar, Listbox, Scrollbar
+from tkinter import Label, Listbox, PhotoImage, Scrollbar, StringVar
+from tkinter import filedialog as fd
+from tkinter import ttk
+from tkinter.messagebox import showerror, showinfo
+from typing import List
+
+import pyautogui
 from pyautogui import sleep
 from pynput import keyboard, mouse
+
 from classes.action import Action
-from queue import Queue
+from helper.file_helper import parse_path
 
 
 class Pos:
@@ -42,15 +45,28 @@ class ActionForm:
         self.no_steps = 0
         self.macro = None
         self.btn_save = None
+        self.macro_filename = None
         self.record = False
+        self.playing = False
+        self.cnt = 1
+        self.iterations = 2
 
     def inc_steps(self):
         """increment steps by 1"""
         self.no_steps += 1
 
-    def on_release(self, key) -> None:
-        x, y = pyautogui.position()
-        self.position_str = f"[{str(x).rjust(4)},{str(y).rjust(4)}]"
+    def reset_cnt(self):
+        self.cnt = 1
+
+    def on_release(self, key: keyboard.Key) -> None:
+        try:
+            if key == key.esc:
+                self.record = False
+                self.playing = False
+                self.reset_cnt()
+                print("Record Stoped")
+        except Exception as ex:
+            print(ex)
 
     def on_click(self, x, y, button: mouse.Button, pressed: bool) -> None:
         if not pressed:
@@ -58,31 +74,36 @@ class ActionForm:
 
         if not self.record:
             return
-
+        # FIX ME ignore whole app window instead of one button
         btnx, btny = self.btn_save.winfo_rootx(), self.btn_save.winfo_rooty()
         btn_size_x = self.btn_save.winfo_width()
         btn_size_y = self.btn_save.winfo_height()
-        print("xxxxx btn pos")
-        print(btnx)
-        print(btny)
-        print("xxxxx inside btn")
-        print(btnx + btn_size_x)
-        print(btny + btn_size_y)
-        print("xxxxx")
-        print(x, y)
-
-        if x > btnx and x < (x + btn_size_x) and y > btny and y < (y + btn_size_y):
+        print(f"{x} > {btnx} and {x} < ({btnx + btn_size_x}) and {y} > {btny} and {y} < ({btny + btn_size_y})")
+        print("size")
+        print(btn_size_x, btn_size_y)
+        print(f"btnx {btnx} btny {btny}")
+        print(x > btnx)
+        print(x < (x + btn_size_x))
+        print(y > btny)
+        print(y < (y + btn_size_y))
+        # if cursor is inside save btn
+        if x > btnx and x < (btnx + btn_size_x) and y > btny and y < (btny + btn_size_y):
             print("ignore button save click")
             return
+        
 
+        print("add to list")
         click = MyClick(x, y, button, pressed)
         self.add_action(click)
-        self.position_str = f"[{str(x).rjust(4)},{str(y).rjust(4)}]"
+        self.position_str = f"Click [{str(x).rjust(4)},{str(y).rjust(4)}]"
+        
 
     def tk_on_click_save(self):
         self.write_to_file()
+        print("Save")
 
     def tk_on_click_play(self):
+        self.playing = True
         print("Play")
         self.read_macro()
         self.play_macro()
@@ -90,11 +111,37 @@ class ActionForm:
     def tk_on_click_clear(self):
         self.actions_list = []
         self.position_str = ""
+        self.no_steps = 0
         print("Clear Macro")
+
+    def tk_on_click_load(self):
+        filename = fd.askopenfilename()
+        print(filename)
+        if filename is not None:
+            self.macro_filename = filename
+        else:
+            print("no file selected")
+
+        self.read_macro()
+        """  filetypes = (
+            ('text files', '*.txt'),
+            ('All files', '*.*')
+        )
+
+        filename = fd.askopenfilename(
+            title='Open a file',
+            initialdir='/',
+            filetypes=filetypes)
+
+        showinfo(
+            title='Selected File',
+            message=filename
+        ) """
 
     def add_action(self, action: ActionUI):
         self.inc_steps()
         action.step_no = self.no_steps
+        # needs to be dict to be saved later as json
         self.actions_list.append(action.to_dict())
 
     def write_to_file(self):
@@ -104,15 +151,38 @@ class ActionForm:
             f.write("\n")
 
     def read_macro(self):
-        with open("teste.json", "r") as f:
+        if self.macro_filename is None and len(self.actions_list) > 0:
+            self.actions_list = self.actions_list_from(dict(steps=self.actions_list))
+            return
+        else:
+            showerror(title="No file selected yet.", message="File not Loaded.")
+
+        with open(self.macro_filename, "r") as f:
             self.macro = json.loads(f.read())
+        self.actions_list = self.actions_list_from(self.macro)
+
+        if self.playing:
+            return
+        # populate listbox
+        for a in self.actions_list:
+            position_str = f"Click [{str(a.x).rjust(4)},{str(a.y).rjust(4)}]"
+            listbox_widget.insert(tk.END, position_str)
 
     def play_macro(self):
-        action = Action()
-        actions = self.actions_list_from(self.macro)
-        for a in actions:
-            print(a.step_no)
-            action.go_click(a.x, a.y, 2, pyautogui.easeInBack)
+        self.record = False
+        action_gui = Action()
+        while self.playing:
+            if self.cnt > self.iterations:
+                break
+            for a in self.actions_list:
+                print(f"step: {a.step_no}")
+                action_gui.go_click(a.x, a.y, 2, pyautogui.easeInBack)
+            self.cnt += 1
+            print(f"macro cnt {self.cnt}/{self.iterations}")
+        time.sleep(0.5)
+        self.playing = False
+        self.reset_cnt()  # reset
+        showinfo(title="Macro", message="Finished!")
 
     def actions_list_from(self, param: dict) -> List[ActionUI]:
         actions = []
@@ -128,15 +198,23 @@ class ActionForm:
                     en = mouse.Button.right
 
             click = MyClick(step["x"], step["y"], mouse.Button(en), step["is_pressed"])
+            click.step_no = step["step_no"]
             actions.append(click)
         return actions
+
+    def set_record_ON(self):
+        self.record = True
+        print("Record On")
+
+    def set_record_OFF(self):
+        self.record = False
 
 
 def update_gui():
     if len(action_ui.actions_list) == 0:
         listbox_widget.delete(0, tk.END)
 
-    if action_ui.position_str != "":
+    if action_ui.position_str != "" and action_ui.record:
         listbox_widget.insert(tk.END, action_ui.position_str)
     action_ui.position_str = ""
     root.after(100, update_gui)
@@ -144,6 +222,8 @@ def update_gui():
 
 if __name__ == "__main__":
     root = tk.Tk(className="JXTECH")
+    root.resizable(False, False)
+
     root.geometry("400x200")
     action_ui = ActionForm()
 
@@ -161,6 +241,8 @@ if __name__ == "__main__":
     bottom_pane.columnconfigure(0, weight=3)
     bottom_pane.columnconfigure(1, weight=2)
     bottom_pane.columnconfigure(2, weight=2)
+    bottom_pane.columnconfigure(3, weight=2)
+    bottom_pane.columnconfigure(4, weight=2)
 
     # BTN SAVE
     btn_save = ttk.Button(root, text="Save Macro", command=action_ui.tk_on_click_save)
@@ -179,10 +261,27 @@ if __name__ == "__main__":
         row=1,
         sticky=tk.S,
     )
+    btn_load_macro = ttk.Button(
+        root, text="Load Macro", command=action_ui.tk_on_click_load
+    )
+    btn_load_macro.grid(
+        column=2,
+        row=1,
+        sticky=tk.S,
+    )
+    rec_image = PhotoImage(file=parse_path("./src/assets/record.png"))
+    btn_record = ttk.Button(
+        root, text="Rec", command=lambda: action_ui.set_record_ON(), image=rec_image
+    )
+    btn_record.grid(
+        column=3,
+        row=1,
+        sticky=tk.S,
+    )
 
     btn_clear = ttk.Button(root, text="Clear", command=action_ui.tk_on_click_clear)
     btn_clear.grid(
-        column=2,
+        column=4,
         row=1,
         sticky=tk.S,
     )
@@ -195,6 +294,8 @@ if __name__ == "__main__":
 
     bottom_pane.add(btn_save)
     bottom_pane.add(btn_play)
+    bottom_pane.add(btn_load_macro)
+    bottom_pane.add(btn_record)
     bottom_pane.add(btn_clear)
     # add to main pane
     main.add(top_pane)
