94e6cac3362e39bfacc4c91ac594986c6b9fded5
Jonnas Costa <jonn567@gmail.com>
2022-11-13 12:06:43 +0000

    move folder to root

--- helper/file_helper.py
+++ helper/file_helper.py
@@ -1,4 +1,5 @@
 from pathlib import Path
 
 def parse_path(path_str: str) -> str:
+    print(str(Path(path_str)))
     return str(Path(path_str))
--- macro_jx.py
+++ macro_jx.py
@@ -1,7 +1,7 @@
 import json
 import time
 import tkinter as tk
-from tkinter import BOTH, Label, Listbox, PhotoImage, Scrollbar, StringVar
+from tkinter import BOTH, Listbox, PhotoImage, Scrollbar
 from tkinter import filedialog as fd
 from tkinter import ttk
 from tkinter.messagebox import showerror, showinfo
@@ -29,6 +29,108 @@ class ActionForm:
         self.cnt = 1
         self.iterations = 2
         self.speed = 1
+        self.root = tk.Tk(className="JXTECH")
+        self.root.resizable(False, False)
+        self.root.geometry("400x245")
+        self.create_widgets()
+        self.update_gui()
+        self.root.mainloop()
+
+    def create_widgets(self):
+        my_notebook = ttk.Notebook(self.root)
+        my_notebook.pack(expand=1, fill=BOTH)
+        # Create Tabs
+        macro_tab = ttk.Frame(my_notebook)
+        my_notebook.add(macro_tab, text="Macro")
+
+        tab2 = ttk.Frame(my_notebook)
+        my_notebook.add(tab2, text="Settings")
+
+        # panel
+        top_pane = tk.PanedWindow(macro_tab, background="#99fb99")
+        main = tk.PanedWindow(macro_tab, background="#99fb99")
+        bottom_pane = tk.PanedWindow(macro_tab, background="#cccb99")
+
+        scrollbar = Scrollbar(macro_tab)
+        self.listbox_widget = Listbox(macro_tab, yscrollcommand=scrollbar.set)
+        self.listbox_widget.pack(side="top", fill="x")
+        top_pane.add(self.listbox_widget)
+
+        bottom_pane.columnconfigure(0, weight=3)
+        bottom_pane.columnconfigure(1, weight=2)
+        bottom_pane.columnconfigure(2, weight=2)
+        bottom_pane.columnconfigure(3, weight=2)
+        bottom_pane.columnconfigure(4, weight=2)
+
+        # BTN SAVE
+        btn_save = ttk.Button(
+            macro_tab, text="Save Macro", command=self.tk_on_click_save
+        )
+        btn_save.grid(
+            column=0,
+            row=1,
+            sticky=tk.S,
+        )
+        # bind is when you want to select a specific listener like the left or right mouse button
+        # btn_save.bind("<Button-1>", action_ui.tk_on_click_save)
+        self.btn_save = btn_save
+        # PLAY
+        btn_play = ttk.Button(
+            macro_tab, text="Play Macro", command=self.tk_on_click_play
+        )
+        btn_play.grid(
+            column=1,
+            row=1,
+            sticky=tk.S,
+        )
+        btn_load_macro = ttk.Button(
+            macro_tab, text="Load Macro", command=self.tk_on_click_load
+        )
+        btn_load_macro.grid(
+            column=2,
+            row=1,
+            sticky=tk.S,
+        )
+        self.rec_image = PhotoImage(file=parse_path("./assets/record.png"))
+        btn_record = ttk.Button(
+            macro_tab,
+            text="Rec",
+            command=lambda: self.set_record_ON(),
+            image=self.rec_image,
+        )
+        btn_record.grid(
+            column=3,
+            row=1,
+            sticky=tk.S,
+        )
+
+        btn_clear = ttk.Button(macro_tab, text="Clear", command=self.tk_on_click_clear)
+        btn_clear.grid(
+            column=4,
+            row=1,
+            sticky=tk.S,
+        )
+
+        scrollbar.config(command=self.listbox_widget.yview)
+
+        bottom_pane.add(btn_save)
+        bottom_pane.add(btn_play)
+        bottom_pane.add(btn_load_macro)
+        bottom_pane.add(btn_record)
+        bottom_pane.add(btn_clear)
+        # add to main pane
+        main.add(top_pane)
+        main.add(bottom_pane)
+        # add to assemble
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
+        top_pane.grid(row=0, column=0, sticky="ew")
+        main.grid(row=1, column=0, sticky="nsew")
+
+        keyboard_listener = keyboard.Listener(on_release=self.on_release)
+        keyboard_listener.start()
+        mouse_listener = mouse.Listener(on_click=self.on_click)
+        mouse_listener.start()
 
     def inc_steps(self):
         """increment steps by 1"""
@@ -48,7 +150,7 @@ class ActionForm:
             print(ex)
 
     def on_click(self, x, y, button: mouse.Button, pressed: bool) -> None:
-        if not pressed:
+        if not pressed: # avoid on release
             return
 
         if not self.record:
@@ -67,7 +169,8 @@ class ActionForm:
         print(x < (x + btn_size_x))
         print(y > btny)
         print(y < (y + btn_size_y))
-        # if cursor is inside save btn
+        # if cursor is inside save btn 
+        # TODO IGNORE WHOLE TK WINDOW
         if (
             x > btnx
             and x < (btnx + btn_size_x)
@@ -107,7 +210,6 @@ class ActionForm:
             print("no file selected")
 
         self.read_macro()
-  
 
     def add_action(self, action: ActionUI):
         self.inc_steps()
@@ -117,27 +219,31 @@ class ActionForm:
 
     def write_to_file(self):
         with open("teste.json", "w") as f:
-            json_file = dict(steps=self.actions_list)
+            aux_ist = [x.to_dict() for x in self.actions_list]
+            json_file = dict(steps=aux_ist)
             f.write(json.dumps(json_file))
-            f.write("\n")
 
     def read_macro(self):
-        if self.macro_filename is None and len(self.actions_list) > 0:
-            self.actions_list = self.actions_list_from(dict(steps=self.actions_list))
-            return
-        else:
-            showerror(title="No file selected yet.", message="File not Loaded.")
-
-        with open(self.macro_filename, "r") as f:
-            self.macro = json.loads(f.read())
-        self.actions_list = self.actions_list_from(self.macro)
-
         if self.playing:
             return
-        # populate listbox
-        for a in self.actions_list:
-            position_str = f"Click [{str(a.x).rjust(4)},{str(a.y).rjust(4)}]"
-            listbox_widget.insert(tk.END, position_str)
+
+        if (self.macro_filename is None or self.macro_filename == "") and len(
+            self.actions_list
+        ) > 0:
+            self.actions_list = self.actions_list_from(dict(steps=self.actions_list))
+            return
+        elif self.macro_filename != "" or self.macro_filename is not None:
+            # READ
+            with open(self.macro_filename, "r") as f:
+                self.macro = json.loads(f.read())
+                self.actions_list = self.actions_list_from(self.macro)
+                # populate listbox
+                for a in self.actions_list:
+                    position_str = f"Click [{str(a.x).rjust(4)},{str(a.y).rjust(4)}]"
+                    self.listbox_widget.insert(tk.END, position_str)
+        else:
+            showerror(title="No file selected yet.", message="File not Loaded.")
+            return
 
     def play_macro(self):
         self.record = False
@@ -180,113 +286,15 @@ class ActionForm:
     def set_record_OFF(self):
         self.record = False
 
+    def update_gui(self):
+        if len(self.actions_list) == 0:
+            self.listbox_widget.delete(0, tk.END)
 
-def update_gui():
-    if len(action_ui.actions_list) == 0:
-        listbox_widget.delete(0, tk.END)
-
-    if action_ui.position_str != "" and action_ui.record:
-        listbox_widget.insert(tk.END, action_ui.position_str)
-    action_ui.position_str = ""
-    root.after(100, update_gui)
+        if self.position_str != "" and self.record:
+            self.listbox_widget.insert(tk.END, self.position_str)
+        self.position_str = ""
+        self.root.after(100, self.update_gui)
 
 
 if __name__ == "__main__":
-    root = tk.Tk(className="JXTECH")
-    root.resizable(False, False)
-
-    root.geometry("400x245")
-    action_ui = ActionForm()
-    
-    my_notebook= ttk.Notebook(root)
-    my_notebook.pack(expand=1,fill=BOTH)
-    #Create Tabs
-    macro_tab= ttk.Frame(my_notebook)
-    my_notebook.add(macro_tab, text= "Macro")
-    
-    tab2= ttk.Frame(my_notebook)
-    my_notebook.add(tab2, text= "Settings")
-    
-    # panel
-    top_pane = tk.PanedWindow(macro_tab, background="#99fb99")
-    main = tk.PanedWindow(macro_tab, background="#99fb99")
-    bottom_pane = tk.PanedWindow(macro_tab, background="#cccb99")
-
-    listbox_entries = []
-    scrollbar = Scrollbar(macro_tab)
-    listbox_widget = Listbox(macro_tab, yscrollcommand=scrollbar.set)
-    listbox_widget.pack(side="top", fill="x")
-    top_pane.add(listbox_widget)
-
-    bottom_pane.columnconfigure(0, weight=3)
-    bottom_pane.columnconfigure(1, weight=2)
-    bottom_pane.columnconfigure(2, weight=2)
-    bottom_pane.columnconfigure(3, weight=2)
-    bottom_pane.columnconfigure(4, weight=2)
-
-    # BTN SAVE
-    btn_save = ttk.Button(macro_tab, text="Save Macro", command=action_ui.tk_on_click_save)
-    btn_save.grid(
-        column=0,
-        row=1,
-        sticky=tk.S,
-    )
-    # bind is when you want to select a specific listener like the left or right mouse button
-    # btn_save.bind("<Button-1>", action_ui.tk_on_click_save)
-    action_ui.btn_save = btn_save
-    # PLAY
-    btn_play = ttk.Button(macro_tab, text="Play Macro", command=action_ui.tk_on_click_play)
-    btn_play.grid(
-        column=1,
-        row=1,
-        sticky=tk.S,
-    )
-    btn_load_macro = ttk.Button(
-        macro_tab, text="Load Macro", command=action_ui.tk_on_click_load
-    )
-    btn_load_macro.grid(
-        column=2,
-        row=1,
-        sticky=tk.S,
-    )
-    rec_image = PhotoImage(file=parse_path("./src/assets/record.png"))
-    btn_record = ttk.Button(
-        macro_tab, text="Rec", command=lambda: action_ui.set_record_ON(), image=rec_image
-    )
-    btn_record.grid(
-        column=3,
-        row=1,
-        sticky=tk.S,
-    )
-
-    btn_clear = ttk.Button(macro_tab, text="Clear", command=action_ui.tk_on_click_clear)
-    btn_clear.grid(
-        column=4,
-        row=1,
-        sticky=tk.S,
-    )
-
-    scrollbar.config(command=listbox_widget.yview)
-
-    bottom_pane.add(btn_save)
-    bottom_pane.add(btn_play)
-    bottom_pane.add(btn_load_macro)
-    bottom_pane.add(btn_record)
-    bottom_pane.add(btn_clear)
-    # add to main pane
-    main.add(top_pane)
-    main.add(bottom_pane)
-    # add to assemble
-    root.grid_rowconfigure(0, weight=1)
-    root.grid_columnconfigure(0, weight=1)
-    top_pane.grid(row=0, column=0, sticky="ew")
-    main.grid(row=1, column=0, sticky="nsew")
-
-    keyboard_listener = keyboard.Listener(on_release=action_ui.on_release)
-    keyboard_listener.start()
-    mouse_listener = mouse.Listener(on_click=action_ui.on_click)
-    mouse_listener.start()
-
-    # Collect events until released
-    update_gui()
-    root.mainloop()
+    ActionForm()
