9abd294ac6c7b3180a26615e732d60d6ef4f9ed3
Jonnas Costa <jonnas.costa@cartrack.com>
2022-11-19 13:19:11 +0000

    add set_txt_status to util

--- util/util.py
+++ util/util.py
@@ -1,8 +1,14 @@
+import tkinter as tk
 from typing import Any
+
+
 def get_setting(val: str, param: dict) -> Any | None:
     if val in param:
         return param[val]
     else:
         return None
 
-    
+
+def set_txt_status(self, text: str):
+    self.txt_status.delete("1.0", tk.END)  # Clear current contents.
+    self.txt_status.insert(tk.INSERT, text)
