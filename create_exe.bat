a9a53414eace4905fd7e899023bb15250005a8c6
Jonnas Costa <jonn567@gmail.com>
2022-11-30 23:02:29 +0000

    add pyinstaller and script

---
+++ create_exe.bat
@@ -0,0 +1,11 @@
+REM change the path below to the your correct path
+pyinstaller -F --onedir --add-data C:\Python311\tcl\tix8.4.3;tcl\tix8.4.3 macro_jx.py -y 
+
+REM NOW mo to the dest folder
+cp -R assets dist\
+mkdir dist\macro
+
+ECHO Running macro_jx.exe
+start dist\macro_jx.exe
+echo "DONE"
+
